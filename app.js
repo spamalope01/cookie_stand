8b204982da6f2b8a134191680582e503de6654e0
spamalope01 <spamalope01@yahoo.com>
2016-04-23 17:23:06 -0700

    all stores added and working as expected.

--- app.js
+++ app.js
@@ -199,6 +199,57 @@ var bellevueSqStore = {
   }
 };
 
+// Alki Store Object
+var alkiStore = {
+  storeName: "Alki",
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 2.6,
+  totals: [],
+  grandTotal: 0,
+  hours: [
+    "10am: ",
+    "11am: ",
+    "12pm: ",
+    "1pm: ",
+    "2pm: ",
+    "3pm: ",
+    "4pm: ",
+    "5pm: "
+],
+
+  getRandom: function() {
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+  },
+
+  hourTotal: function() {
+  for(var j = 0; j < this.hours.length; j++) {
+    var ran = Math.floor(this.getRandom() * this.avgCookie);
+    this.totals.push(ran);
+    this.grandTotal += ran;
+  }
+  },
+
+  render: function() {
+  this.hourTotal();
+  var pEl = document.getElementById("alki_Store");
+  var ulEl = document.createElement('ul');
+
+  for (var i = 0; i < this.hours.length; i++) {
+    var liEl = document.createElement('li');
+    pEl.textContent = this.storeName;
+    liEl.textContent = this.hours[i] + this.totals[i];
+    ulEl.appendChild(liEl);
+  }
+  var liMent = document.createElement('li');
+  liMent.textContent = "TOTALS " + this.grandTotal;
+  ulEl.appendChild(liMent);
+  pEl.appendChild(ulEl);
+
+  }
+};
 pikeStore.render();
 seaTacStore.render();
 southCenterStore.render();
+bellevueSqStore.render();
+alkiStore.render();
