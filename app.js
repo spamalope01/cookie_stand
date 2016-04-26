3c4d590e0795ebd8e0f3a42b6373cbc44107ff4c
spamalope01 <spamalope01@yahoo.com>
2016-04-25 20:08:35 -0700

    scott monkeyed with my code, tried to dry it out.

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // Pike Place Store Object
-
+var hours = [
+  "10am: ",
+  "11am: ",
+  "12pm: ",
+  "1pm: ",
+  "2pm: ",
+  "3pm: ",
+  "4pm: ",
+  "5pm: "
+];
 var pikeStore = {
   storeName: "Pike Place",
   minCust: 17,
@@ -7,46 +16,38 @@ var pikeStore = {
   avgCookie: 5.2,
   totals: [],
   grandTotal: 0,
-  hours: [
-    "10am: ",
-    "11am: ",
-    "12pm: ",
-    "1pm: ",
-    "2pm: ",
-    "3pm: ",
-    "4pm: ",
-    "5pm: "
-],
 
-  getRandom: function() {
-    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+  getRandom: function(min, max) {
+    return Math.random() * (max - min) + min;
   },
 
-  hourTotal: function() {
-  for(var j = 0; j < this.hours.length; j++) {
-    var ran = Math.floor(this.getRandom() * this.avgCookie);
-    this.totals.push(ran);
-    this.grandTotal += ran;
-  }
+  hourTotal: function(arr, totals, avg, rand, hour) {
+    console.log(totals);
+    for(var j = 0; j < hour.length; j++) {
+      var ran = Math.floor(rand(this.minCust, this.maxCust) * avg);
+      arr.push(ran);
+      console.log(totals, ran);
+      totals += ran;
+    }
   },
 
   render: function() {
-  this.hourTotal();
-  var pEl = document.getElementById("pikePlStore");
-  var ulEl = document.createElement('ul');
-
-  for (var i = 0; i < this.hours.length; i++) {
-    var liEl = document.createElement('li');
+    this.hourTotal(this.totals, this.grandTotal, this.avgCookie, this.getRandom, hours);
+    var pEl = document.getElementById("pikePlStore");
+    var ulEl = document.createElement('ul');
     pEl.textContent = this.storeName;
-    liEl.textContent = this.hours[i] + this.totals[i];
-    ulEl.appendChild(liEl);
-  }
-  var liMent = document.createElement('li');
-  liMent.textContent = "TOTALS " + this.grandTotal;
-  ulEl.appendChild(liMent);
-  pEl.appendChild(ulEl);
 
-  }
+    for (var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + this.totals[i];
+      ulEl.appendChild(liEl);
+    }
+    var liMent = document.createElement('li');
+    liMent.textContent = "TOTALS " + this.grandTotal;
+    ulEl.appendChild(liMent);
+    pEl.appendChild(ulEl);
+
+    }
 };
 
 // SeaTac Store Object
@@ -248,8 +249,20 @@ var alkiStore = {
 
   }
 };
-pikeStore.render();
-seaTacStore.render();
-southCenterStore.render();
-bellevueSqStore.render();
-alkiStore.render();
+
+var stores = [
+  pikeStore,
+  alkiStore,
+  seaTacStore,
+  bellevueSqStore,
+  southCenterStore
+];
+for(var k = 0; k < stores.length; k++) {
+  stores[k].render();
+}
+
+// pikeStore.render();
+// seaTacStore.render();
+// southCenterStore.render();
+// bellevueSqStore.render();
+// alkiStore.render();
