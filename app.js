cc7333cc8085af819003824cb4ab73a589e49141
spamalope01 <spamalope01@yahoo.com>
2016-04-30 11:49:40 -0700

    got the constructor project working.  added stylesheet.css for styling on the styleguide.html.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Pike Place Store Object
 var hours = [
   '10am: ',
   '11am: ',
@@ -13,13 +12,13 @@ var hours = [
 Store.prototype.totals = [];
 Store.prototype.grandTotal = 0;
 
-function Store (storeName, minCust, maxCust, avgCookie) {
+function Store (storeName, minCust, maxCust, avgCookie, id) {
   this.storeName = storeName;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookie = avgCookie;
+  this.storeId = id;
 };
-
 
 Store.prototype.getRandom = function(min, max) {
   return Math.random() * (max - min) + min;
@@ -36,7 +35,7 @@ Store.prototype.hourTotal = function(arr, total, avg, rand, hour) {
 
 Store.prototype.render = function() {
   Store.prototype.grandTotal = this.hourTotal(this.totals, this.grandTotal, this.avgCookie, this.getRandom, hours);
-  var pEl = document.getElementById('pikePlStore');
+  var pEl = document.getElementById(this.storeId);
   var ulEl = document.createElement('ul');
   pEl.textContent = this.storeName;
 
@@ -52,17 +51,11 @@ Store.prototype.render = function() {
 
 };
 
-var pikeStore = new Store('Pike Place', 17, 88, 5.2);
-var seaTacStore = new Store('SeaTac Airport', 6, 24, 1.2);
-var southCenterStore = new Store('South Center', 11, 38, 1.9);
-var bellevueSqStore = new Store('Bellevue Square', 20, 48, 3.3);
-var alkiStore = new Store('Alki', 3, 24, 2.6);
-
-// pikeStore.render();
-// seaTacStore.render();
-// southCenterStore.render();
-// bellevueSqStore.render();
-// alkiStore.render();
+var pikeStore = new Store('Pike Place', 17, 88, 5.2, 'pikeplace');
+var seaTacStore = new Store('SeaTac Airport', 6, 24, 1.2, 'seaTacStore');
+var southCenterStore = new Store('South Center', 11, 38, 1.9, 'southCtrStore');
+var bellevueSqStore = new Store('Bellevue Square', 20, 48, 3.3, 'bellSqStore');
+var alkiStore = new Store('Alki', 3, 24, 2.6, 'alki_Store');
 
 var stores = [
   pikeStore,
@@ -74,180 +67,3 @@ var stores = [
 for(var k = 0; k < stores.length; k++) {
   stores[k].render();
 }
-// need to figure out what i have to pass in as arguments when calling pikeStore.render.  that is the first domino, so what do i pass in...how do i get values to pass in and flow through the other methods?   and right now line 51 is giving me 'avgCookie' is not defined when i refresh the browser.
-
-
-
-
-// // SeaTac Store Object
-// var seaTacStore = {
-//   storeName: 'SeaTac Airport',
-//   minCust: 6,
-//   maxCust: 24,
-//   avgCookie: 1.2,
-//   totals: [],
-//   grandTotal: 0,
-//
-//   getRandom: function(min, max) {
-//     return Math.random() * (max - min) + min;
-//   },
-//
-//   hourTotal: function(arr, total, avg, rand, hour) {
-//     console.log(total);
-//     for(var j = 0; j < hour.length; j++) {
-//       var ran = Math.floor(rand(this.minCust, this.maxCust) * avg);
-//       arr.push(ran);
-//       console.log(total, ran, this.grandTotal);
-//       total += ran;
-//     }
-//     return total;
-//   },
-//
-//   render: function() {
-//     this.grandTotal = this.hourTotal(this.totals, this.grandTotal, this.avgCookie, this.getRandom, hours);
-//     var pEl = document.getElementById('seaTacStore');
-//     var ulEl = document.createElement('ul');
-//     pEl.textContent = this.storeName;
-//
-//     for (var i = 0; i < hours.length; i++) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[i] + this.totals[i];
-//       ulEl.appendChild(liEl);
-//     }
-//     var liMent = document.createElement('li');
-//     liMent.textContent = 'TOTALS ' + this.grandTotal;
-//     ulEl.appendChild(liMent);
-//     pEl.appendChild(ulEl);
-//
-//   }
-// };
-//
-// // Southcenter Store Object
-// var southCenterStore = {
-//   storeName: 'South Center',
-//   minCust: 11,
-//   maxCust: 38,
-//   avgCookie: 1.9,
-//   totals: [],
-//   grandTotal: 0,
-//
-//   getRandom: function(min, max) {
-//     return Math.random() * (max - min) + min;
-//   },
-//
-//   hourTotal: function(arr, total, avg, rand, hour) {
-//     console.log(total);
-//     for(var j = 0; j < hour.length; j++) {
-//       var ran = Math.floor(rand(this.minCust, this.maxCust) * avg);
-//       arr.push(ran);
-//       console.log(total, ran, this.grandTotal);
-//       total += ran;
-//     }
-//     return total;
-//   },
-//
-//   render: function() {
-//     this.grandTotal = this.hourTotal(this.totals, this.grandTotal, this.avgCookie, this.getRandom, hours);
-//     var pEl = document.getElementById('southCtrStore');
-//     var ulEl = document.createElement('ul');
-//     pEl.textContent = this.storeName;
-//
-//     for (var i = 0; i < hours.length; i++) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[i] + this.totals[i];
-//       ulEl.appendChild(liEl);
-//     }
-//     var liMent = document.createElement('li');
-//     liMent.textContent = 'TOTALS ' + this.grandTotal;
-//     ulEl.appendChild(liMent);
-//     pEl.appendChild(ulEl);
-//
-//   }
-// };
-//
-// // Bellevue Square Store Object
-// var bellevueSqStore = {
-//   storeName: 'Bellevue Square',
-//   minCust: 20,
-//   maxCust: 48,
-//   avgCookie: 3.3,
-//   totals: [],
-//   grandTotal: 0,
-//
-//   getRandom: function(min, max) {
-//     return Math.random() * (max - min) + min;
-//   },
-//
-//   hourTotal: function(arr, total, avg, rand, hour) {
-//     console.log(total);
-//     for(var j = 0; j < hour.length; j++) {
-//       var ran = Math.floor(rand(this.minCust, this.maxCust) * avg);
-//       arr.push(ran);
-//       console.log(total, ran, this.grandTotal);
-//       total += ran;
-//     }
-//     return total;
-//   },
-//
-//   render: function() {
-//     this.grandTotal = this.hourTotal(this.totals, this.grandTotal, this.avgCookie, this.getRandom, hours);
-//     var pEl = document.getElementById('bellSqStore');
-//     var ulEl = document.createElement('ul');
-//     pEl.textContent = this.storeName;
-//
-//     for (var i = 0; i < hours.length; i++) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[i] + this.totals[i];
-//       ulEl.appendChild(liEl);
-//     }
-//     var liMent = document.createElement('li');
-//     liMent.textContent = 'TOTALS ' + this.grandTotal;
-//     ulEl.appendChild(liMent);
-//     pEl.appendChild(ulEl);
-//
-//   }
-// };
-//
-// // Alki Store Object
-// var alkiStore = {
-//   storeName: 'Alki',
-//   minCust: 3,
-//   maxCust: 24,
-//   avgCookie: 2.6,
-//   totals: [],
-//   grandTotal: 0,
-//
-//   getRandom: function(min, max) {
-//     return Math.random() * (max - min) + min;
-//   },
-//
-//   hourTotal: function(arr, total, avg, rand, hour) {
-//     console.log(total);
-//     for(var j = 0; j < hour.length; j++) {
-//       var ran = Math.floor(rand(this.minCust, this.maxCust) * avg);
-//       arr.push(ran);
-//       console.log(total, ran, this.grandTotal);
-//       total += ran;
-//     }
-//     return total;
-//   },
-//
-//   render: function() {
-//     this.grandTotal = this.hourTotal(this.totals, this.grandTotal, this.avgCookie, this.getRandom, hours);
-//     var pEl = document.getElementById('alki_Store');
-//     var ulEl = document.createElement('ul');
-//     pEl.textContent = this.storeName;
-//
-//     for (var i = 0; i < hours.length; i++) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[i] + this.totals[i];
-//       ulEl.appendChild(liEl);
-//     }
-//     var liMent = document.createElement('li');
-//     liMent.textContent = 'TOTALS ' + this.grandTotal;
-//     ulEl.appendChild(liMent);
-//     pEl.appendChild(ulEl);
-//
-//   }
-// };
-//
